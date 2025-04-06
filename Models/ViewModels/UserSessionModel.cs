0989092fb100829018932264978a211ddb4a3a08
현호 백 <baik5634@naver.com>
2025-04-06 23:56:13 +0900

    refactor : 기본 모델 값 추가

--- Models/ViewModels/UserSessionModel.cs
+++ Models/ViewModels/UserSessionModel.cs
@@ -3,7 +3,7 @@ namespace BoardCreate.Models.ViewModels
     public class UserSessionModel
     {
         public string NickName { get; set; }
-        public string UserID { get; set; }
+        public string UserID { get; set; } = "guest";
         public int IDX { get; set; }
         public int UserLevel { get; set; }
     }
